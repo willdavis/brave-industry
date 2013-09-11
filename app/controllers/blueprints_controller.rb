1353f71ad6de021656367d341c5458c00e6fc90c
willdavis <willdavis84@gmail.com>
2013-09-11 00:23:22 -0700

    updated blueprint show ui

--- app/controllers/blueprints_controller.rb
+++ app/controllers/blueprints_controller.rb
@@ -1,14 +1,18 @@
 class BlueprintsController < ApplicationController
   def index
-  	connection = Faraday.new(:url => "http://evedata.herokuapp.com") do |conn|
+  	@blueprints = evedata.get("/blueprints").body
+  end
+
+  def show
+  	@blueprint = evedata.get("/blueprints/#{params[:id]}").body.first
+  end
+  
+  private
+  def evedata
+  	Faraday.new(:url => "http://evedata.herokuapp.com") do |conn|
   		conn.request :json
   		conn.response :json, :content_type => /\bjson$/
   		conn.adapter Faraday.default_adapter
   	end
-  	
-  	@blueprints = connection.get("/blueprints").body
-  end
-
-  def show
   end
 end
