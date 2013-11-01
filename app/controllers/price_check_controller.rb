d12bfc9bc0276af87fbf6189d106a77de5183551
willdavis <bdavis84@pacbell.net>
2013-10-31 23:29:41 -0700

    xml now parsed and displayed on view

--- app/controllers/price_check_controller.rb
+++ app/controllers/price_check_controller.rb
@@ -3,6 +3,26 @@ class PriceCheckController < ApplicationController
 	end
 	
   def results
-  	@results = params[:input_file]
+  	@results = Nokogiri::XML params[:input_file]
+  	@fitting = @results.xpath("//fitting").first
+  	@ship = @results.xpath("//shipType").first
+  	
+  	ship_data = Rails.cache.fetch("Items.by_name=#{@ship[:value]}") { evedata.get("/items?name=#{@ship[:value]}").body.first }
+  	@ship[:id] = ship_data["id"]
+  	
+  	@hardware = @results.xpath("//hardware")
+  	@hardware.map do |item|
+  		item_data = Rails.cache.fetch("Items.by_name=#{item[:type]}") { evedata.get("/items?name=#{item[:type]}").body.first }
+  		item[:id] = item_data["id"]
+  	end
+  end
+  
+  private
+  def evedata
+  	Faraday.new(:url => "http://evedata.herokuapp.com") do |conn|
+  		conn.request :json
+  		conn.response :json, :content_type => /\bjson$/
+  		conn.adapter Faraday.default_adapter
+  	end
   end
 end
