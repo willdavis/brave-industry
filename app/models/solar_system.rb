92e529a7ccd53aa2315f2b72dc572410255cc7b5
willdavis <bdavis84@pacbell.net>
2014-03-11 15:34:01 -0700

    added solar_system model

---
+++ app/models/solar_system.rb
@@ -0,0 +1,46 @@
+class SolarSystem
+  # ActiveModel plumbing to make `form_for` work
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+  
+  # ActiveModel support
+  def persisted?; false; end
+  def save!; true; end
+  
+  # ActiveRecord queries
+  def self.all; SolarSystem.get_all_systems; end
+  def self.find(solar_id); SolarSystem.get_system(solar_id); end
+  
+  # Dynamic attributes should match the values supplied by form_for params
+  ATTRIBUTES = [:id, :name]
+  attr_accessor *ATTRIBUTES
+  
+  def initialize(attributes = {})
+    ATTRIBUTES.each do |attribute|
+      send("#{attribute}=", attributes[attribute])
+    end
+  end
+  
+  private
+  
+  def self.get_all_systems
+    systems = Rails.cache.fetch("solar_systems.all", compress: true) { SolarSystem.evedata.get("/solar_systems?limit=5500").body }
+    systems.map{ |system| SolarSystem.new(:id=>system["id"], :name=>system["name"]) }
+  end
+  
+  def self.get_system(solar_id)
+    system = Rails.cache.fetch("solar_system.#{solar_id}") { SolarSystem.evedata.get("/solar_systems/#{solar_id}").body.first }
+    return SolarSystem.new(:id => system["id"], :name => system["name"]) if !system.nil?
+    return SolarSystem.new if system.nil?
+  end
+  
+  def self.evedata
+    @connection ||= Faraday.new(:url => "http://evedata.herokuapp.com") do |conn|
+      conn.request :json
+      conn.response :json, :content_type => /\bjson$/
+      conn.adapter Faraday.default_adapter
+    end
+  end
+  
+end
